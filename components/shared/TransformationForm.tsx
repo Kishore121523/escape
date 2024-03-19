da4ee688fa41ab821fe62b651b2af1d063201b1a
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2024-03-18 23:40:43 -0400

    Added Form comp

---
+++ components/shared/TransformationForm.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { z } from "zod"
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+})
+
+const TransformationForm = () => {
+  return (
+    <div>
+      dsds
+    </div>
+  )
+}
+
+export default TransformationForm
